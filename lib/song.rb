71f549075d9ad9b06fb67ee4fe2e1305ea1ef196
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-04-12 03:32:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ class Song
     @artist = artist
     @genre = genre
     @@count += 1
-    @@artists << @artist
+    @@artists << @artist unless @@artists.include? @artist
     @@genres << @genre
   end
 
